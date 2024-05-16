0f0eaf23f627a7267562c81d44b92c5640424250
supakit-code <it_harem@rmutl.ac.th.>
2024-05-16 14:10:01 +0700

    เพิ่มหน้า home

---
+++ lib/screen/my_home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return const Text("Hello World");
+  }
+}
